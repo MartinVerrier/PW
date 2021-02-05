5a9911a6eeb8a4b0e1db8e37ba08a741adbbe27a
Tom Da Costa <tom.da-costa@etu.univ-grenoble-alpes.fr>
2021-02-05 23:02:20 +0100

    5.5

--- TP1/5_CALEN_TOM/5.php
+++ TP1/5_CALEN_TOM/5.php
@@ -8,17 +8,20 @@
     </head>
     <body>
         <?php 
-            $first = mktime(0,0,0,$_GET["month"],1,$_GET["year"]);
+            $date = explode("-",$_GET["date"]);
+            $first = mktime(0,0,0,$date[1],1,$date[0]);
             $offset = date("N",$first);
+            echo "<h1> " . date("F Y",$first);
             echo "<table><tbody><tr><td>Lundi</td><td>Mardi</td><td>Mercredi</td><td>Jeudi</td><td>Vendredi</td><td>Samedi</td><td>Dimanche</td></tr>";
-            for($i = 2-$offset; $i <= intval(date("t",$first));) {
+            $i = 2-$offset;
+            while($i <= intval(date("t",$first))) {
                 echo "<tr>";
                 for($j = 1; $j <= 7; $j++,$i++) {
                     echo "<td>";
                     if($i >= 1 && $i <= intval(date("t",$first))) {
                         $tmp = getdate(mktime(0,0,0,date("m",$first),$i,date("y",$first)));
                         echo $tmp["mday"];
-                        if($i == intval($_GET["jour"])) {
+                        if($i == intval($date[2])) {
                             echo "</br><span>" . $_GET["event"] . "</span>";
                         }
                     }
