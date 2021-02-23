46a1eed12fe34153bb2aa827dccbafeed6e104ce
Loris Mottino <loris.mottino@etu.univ-grenoble-alpes.fr>
2021-02-23 20:05:44 +0100

    Petit commit

--- TP2/11.php
+++ TP2/11.php
@@ -1,5 +1,18 @@
 <?php
 	
-	
-	
+	$strfile = "Données/DSPE_ANT_GSM_EPSG4326.csv";
+
+	$file = fopen($strfile, "r");
+
+	$count = 0;
+	fgetcsv($file, ",");
+	while (fgetcsv($file, ",") !== FALSE) {
+		$count++;
+	}
+
+	fclose($file);
+
+	echo "Nombre d'antennes référencées : " . $count . "\n";
+
+
 ?>
